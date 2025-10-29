dbf85abb90e4b08a5a4b4b70eb28bbd4b896b5dc
Joost van Rooijen <joost.rzz@gmail.com>
2025-10-29 14:33:25 +0100

    Add method HeadingString to GeoDir.

--- SourceCode/AgOpenGPS.Core/Models/Base/GeoDir.cs
+++ SourceCode/AgOpenGPS.Core/Models/Base/GeoDir.cs
@@ -12,15 +12,36 @@ namespace AgOpenGPS.Core.Models
         public GeoDir(GeoDelta delta)
         {
             AngleInRadians = Math.Atan2(delta.EastingDelta, delta.NorthingDelta);
+            if (AngleInRadians < 0.0) AngleInRadians += 2 * Math.PI;
         }
 
+        // The returned angle will be in the range [0.0 ... 2 * PI >
+        // Also internally, GeoDir will always keep the angle in this range, so it does not need to be checked
+        // for each frame that wants to display the HeadingString.
         public double AngleInRadians { get; }
+
+        // The returned angle will be in the range [0.0 ... 360 >
         public double AngleInDegrees => Units.RadiansToDegrees(AngleInRadians);
 
-        public GeoDir PerpendicularLeft => new GeoDir(AngleInRadians - 0.5 * Math.PI);
+        public GeoDir PerpendicularLeft => new GeoDir(
+            0.5 * Math.PI < AngleInRadians
+            ? AngleInRadians - 0.5 * Math.PI
+            : AngleInRadians + 1.5 * Math.PI);
 
-        public GeoDir PerpendicularRight => new GeoDir(AngleInRadians + 0.5 * Math.PI);
-        public GeoDir Inverted => new GeoDir(AngleInRadians + Math.PI);
+        public GeoDir PerpendicularRight => new GeoDir(
+            1.5 * Math.PI < AngleInRadians
+            ? AngleInRadians - 1.5 * Math.PI
+            : AngleInRadians + 0.5 * Math.PI);
+
+        public GeoDir Inverted => new GeoDir(
+            Math.PI < AngleInRadians
+            ? AngleInRadians - Math.PI
+            : AngleInRadians + Math.PI);
+
+        public string HeadingString(string formatString = "N1")
+        {
+            return AngleInDegrees.ToString(formatString) + "\u00B0";
+        }
 
         public static GeoDelta operator *(double distance, GeoDir dir)
         {
@@ -29,12 +50,19 @@ namespace AgOpenGPS.Core.Models
 
         public static GeoDir operator +(GeoDir dir, double angleInRadians)
         {
-            return new GeoDir(dir.AngleInRadians + angleInRadians);
+            return new GeoDir(NormalizeAngle(dir.AngleInRadians + angleInRadians));
         }
 
         public static GeoDir operator -(GeoDir dir, double angleInRadians)
         {
-            return new GeoDir(dir.AngleInRadians - angleInRadians);
+            return new GeoDir(NormalizeAngle(dir.AngleInRadians - angleInRadians));
+        }
+
+        private static double NormalizeAngle(double angleInRadians)
+        {
+            while (angleInRadians < 0.0) angleInRadians += 2 * Math.PI;
+            while (2 * Math.PI < angleInRadians) angleInRadians -= 2 * Math.PI;
+            return angleInRadians;
         }
 
     }
