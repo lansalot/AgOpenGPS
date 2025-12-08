fe6ed8793b24766b31da0e8696cdf54b40eb9586
Joost van Rooijen <joost.rzz@gmail.com>
2025-12-08 19:33:36 +0100

    Use VehicleDotVisual to harmonize visualization of 'vehicle dot' in forHeadLine

--- SourceCode/GPS/Forms/Guidance/FormHeadLine.cs
+++ SourceCode/GPS/Forms/Guidance/FormHeadLine.cs
@@ -2,6 +2,7 @@
 using AgOpenGPS.Controls;
 using AgOpenGPS.Core.Models;
 using AgOpenGPS.Core.Translations;
+using AgOpenGPS.Core.Visuals;
 using AgOpenGPS.Helpers;
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
@@ -449,14 +450,7 @@ namespace AgOpenGPS
                 }
                 GL.End();
             }
-
-            //the vehicle
-            GL.PointSize(16.0f);
-            GL.Begin(PrimitiveType.Points);
-            GL.Color3(0.95f, 0.190f, 0.20f);
-            GL.Vertex3(mf.pivotAxlePos.easting, mf.pivotAxlePos.northing, 0.0);
-            GL.End();
-
+            VehicleDotVisual.DrawVehicleDot(mf.pivotAxlePos.ToGeoCoord());
             //draw the line building graphics
             if (start != 99999 || end != 99999) DrawABTouchLine();
 
