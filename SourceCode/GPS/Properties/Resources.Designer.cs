8499bfce30568c380b3538ac60b2dc3489525336
Gunics Balázs <gunicsba@gmail.com>
2026-04-05 21:39:17 +0200

    Slightly changed how Task Controller communicates with AgOpenGPS.
    Previously in case we had TC crash or fail to start the user had no idea what's going on.
    Now we have 3 states: We talk with TC but there's no implement.
    We see the implement then SC is OFF / ON.
    
    (cherry picked from commit c0f82b5d8be33bfad5163703bc221ade93429129)

--- SourceCode/GPS/Properties/Resources.Designer.cs
+++ SourceCode/GPS/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace AgOpenGPS.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -2162,6 +2162,16 @@ namespace AgOpenGPS.Properties {
         internal static System.Drawing.Bitmap IsobusSectionControlOn {
             get {
                 object obj = ResourceManager.GetObject("IsobusSectionControlOn", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap IsobusSectionControlIdle {
+            get {
+                object obj = ResourceManager.GetObject("IsobusSectionControlIdle", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
